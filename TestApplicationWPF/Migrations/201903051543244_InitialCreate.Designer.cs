d0cf0c1dc73fafa28f764c5e4d1e132b88613797
Мамедли Тогрул Вугар <Mame_fy31@itstep.edu.az>
2019-03-05 20:08:18 +0400

    MergedProjects

--- TestApplicationWPF/Migrations/201903051543244_InitialCreate.Designer.cs
+++ TestApplicationWPF/Migrations/201903051543244_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace TestApplicationWPF.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class _1stMigration : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(_1stMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201903050643186_1stMigration"; }
+            get { return "201903051543244_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source

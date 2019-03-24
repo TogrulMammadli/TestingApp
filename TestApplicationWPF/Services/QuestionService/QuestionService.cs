e51aac0d44fe2051aa86b41e7eec1a51c60b2fa8
ChaikaV <46184570+ChaikaV@users.noreply.github.com>
2019-03-24 13:53:11 +0400

    CreateTestt

---
+++ TestApplicationWPF/Services/QuestionService/QuestionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplicationWPF.Repository.QuestionsRepository;
+
+namespace TestApplicationWPF.Services.QuestionService
+{
+    public class QuestionService : IQuestionService
+    {
+        public IQuestionRepository QuestionRepository = new QuestionRepository();
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            this.QuestionRepository = questionRepository ?? throw new ArgumentNullException(nameof(QuestionRepository));
+        }
+    }
+}

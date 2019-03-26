6a7b573adf6eee1e6a42f83e5c6dd1aef9fd575b
Togrul Mammadli <mamedlitogrul99@gmail.com>
2019-03-27 00:18:19 +0400

    Added Somethings

--- TestApplicationWPF/Pages/UserManagement.xaml.cs
+++ TestApplicationWPF/Pages/UserManagement.xaml.cs
@@ -25,13 +25,11 @@ namespace TestApplicationWPF.Pages
     /// </summary>
     public partial class UserManagement : Page
     {
-        static UserRepository repository = new UserRepository();
-        UserService UserService = new UserService(repository);
+      
         public UserManagement(User user)
         {
             InitializeComponent();
-            
-            var viewModel = new UserManagementViewModel(UserService);
+            var viewModel = new UserManagementViewModel();
             this.DataContext = viewModel;
         }
 
@@ -72,7 +70,7 @@ namespace TestApplicationWPF.Pages
 
         private void Remove_Click(object sender, RoutedEventArgs e)
         {
-            UserService.RemoveUser(((User)usersListBox.SelectedItems[0]));
+            //UserService.RemoveUser(((User)usersListBox.SelectedItems[0]));
 
         }
     }
---
+++ TestApplicationWPF/ViewModel/UserManagementViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplicationWPF.Command;
+using TestApplicationWPF.Models;
+using TestApplicationWPF.Repository.UserRepository;
+using TestApplicationWPF.Services.UserServices;
+
+namespace TestApplicationWPF.ViewModels
+{
+    public class UserManagementViewModel : BaseViewModel
+    {
+        public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
+
+        public UserManagementViewModel()
+        {
+            //Messenger.Default.Register<NewUserMessage>(this, AddUserExexute);
+        }
+
+
+        //private void AddUserExexute(NewUserMessage message)
+        //{
+        //    this.Users.Add(message.NewUser);
+        //}
+    }
+}
+

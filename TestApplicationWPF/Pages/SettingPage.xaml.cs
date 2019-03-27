1290aad5585816536ea39b9482257ef396c1d7a1
parviz.xuduyev1000@hotmail.com <parviz.xuduyev1000@hotmail.com>
2019-03-28 01:53:43 +0400

    seting page

--- TestApplicationWPF/Pages/SettingPage.xaml.cs
+++ TestApplicationWPF/Pages/SettingPage.xaml.cs
@@ -33,55 +33,25 @@ namespace TestApplicationWPF.Pages
         {
             try
             {
-                if (textboxname.Text.Length <= 1 && textboxname.Text != "")
-                {
-                    TextBlockNameWarning.Text = "Name must be more letters!";                   
-                    textboxname.Clear();
-                }
-                else if (textboxage.Text == "" &&  int.Parse(textboxage.Text) < 7)
-                {
-                    TextBlockAgeWarning.Text = "User must be older than 7 years!";                     
-                    textboxage.Clear();
-                }
-                else if (int.Parse(textboxage.Text) > 70)
-                {
-                    TextBlockAgeWarning.Text = "User must not be older than 70 years!";
-                    textboxage.Clear();
-                }
-                else if (textboxphone.Text != "" && textboxphone.Text.Length < 10)
-                {
-                    TextBlockPhoneWarning.Text = "Length must not be than 10 digits!";
-                    textboxphone.Clear();
-                }
-                else if (!Regex.IsMatch(textboxemail.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase) && textboxemail.Text != "")
-                {
-                    TextBlockEmailWarning.Text = "Wrong e-mail format!";
-                    textboxemail.Clear();
-                }
-                else if (textboxpswd.Text.Length < 8 && textboxconfpswd.Text.Length < 8)
-                {
-                    MessageBox.Show("Минимальная длина пароля 8 символов");
-                    textboxpswd.Clear();
-                    textboxconfpswd.Clear();
-                }
-                else if (textboxpswd.Text != textboxconfpswd.Text)
-                {
-                    MessageBox.Show("Пароли не совпадают");
-                    textboxpswd.Clear();
-                    textboxconfpswd.Clear();
-                }
-                else
-                {
-                    /////////
-                    //////
-                    ///
-                }
+                if (textboxname.Text.Length < 2 && textboxname.Text != ""){TextBlockNameWarning.Text = "Name must be more letters!";}
+                else{TextBlockNameWarning.Text = "";}
 
+                if (int.Parse(textboxage.Text) < 7 && textboxage.Text != ""){TextBlockAgeWarning.Text = "User must be older than 7 years!";}
+                else if (int.Parse(textboxage.Text) > 70 && textboxage.Text != ""){TextBlockAgeWarning.Text = "User must not be older than 70 years!";}
+                else{TextBlockAgeWarning.Text = "";}
             }
-            catch (Exception)
+            catch (Exception){ }
+
+            if (textboxphone.Text != "" && textboxphone.Text.Length < 10){Textboxphonewarning.Text = "Length must not be than 10 digits!";}
+            else{Textboxphonewarning.Text = "";}
+            if (!Regex.IsMatch(textboxemail.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase) && textboxemail.Text != "")
             {
-                throw;
+                TextBlockEmailWarning.Text = "Wrong e-mail format!";
             }
+
+            else{TextBlockEmailWarning.Text = "";}
+            if (textboxpswd.Text != "" && textboxconfpswd.Text != "" && textboxpswd.Text != textboxconfpswd.Text){TextBlockPswdWarning.Text = "Passwords do not match!";}
+            else{TextBlockPswdWarning.Text = "";}
         }
 
         private void Textboxname_PreviewTextInput(object sender, TextCompositionEventArgs e)

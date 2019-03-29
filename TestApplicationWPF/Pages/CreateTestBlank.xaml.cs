4bbff858d4b6a24646bfcb82488b8a5470150798
aliyevnatig80 <aliyevnatig80@gmail.com>
2019-03-29 21:27:28 +0400

    jgjfgjh

--- TestApplicationWPF/Pages/CreateTestBlank.xaml.cs
+++ TestApplicationWPF/Pages/CreateTestBlank.xaml.cs
@@ -32,102 +32,106 @@ namespace TestApplicationWPF.Pages
     /// </summary>
     public partial class CreateTestBlank : Window
     {
-       
 
-
-        TestBlankViewModel testBlankViewModel;
-        TestBlank TestBlk = new TestBlank();
         public CreateTestBlank()
         {
             InitializeComponent();
-            AllQuestionsRadioButton.IsChecked = true;
-            MediumRadioButton.IsChecked = true;
-            UnlimitedTimeRadioButton.IsChecked = true;
-
-            testBlankViewModel = new TestBlankViewModel(new TestService(new TestBlankRepository()), new QuestionService(new QuestionRepository()));
-            this.DataContext = testBlankViewModel;
+            
         }
+
+        //TestBlankViewModel testBlankViewModel;
+        //TestBlank TestBlk = new TestBlank();
+        //public CreateTestBlank()
+        //{
+        //    InitializeComponent();
+        //    AllQuestionsRadioButton.IsChecked = true;
+        //    MediumRadioButton.IsChecked = true;
+        //    UnlimitedTimeRadioButton.IsChecked = true;
+
+        //    testBlankViewModel = new TestBlankViewModel(new TestService(new TestBlankRepository()), new QuestionService(new QuestionRepository()));
+        //    this.DataContext = testBlankViewModel;
+        //}
        
 
-        private void TestBlankQuestioRemove_Click(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.RemoveQuestionFromTestBlank.Execute((Question)(((Button)sender).DataContext));
-        }
+        //private void TestBlankQuestioRemove_Click(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.RemoveQuestionFromTestBlank.Execute((Question)(((Button)sender).DataContext));
+        //}
 
-        private void TestBlankQuestioView_Click(object sender, RoutedEventArgs e)
-        {
-            ViewQuestionInfo viewQuestionInfo = new ViewQuestionInfo((Question)(((Button)sender).DataContext));
-            viewQuestionInfo.ShowDialog();
-        }
+        //private void TestBlankQuestioView_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ViewQuestionInfo viewQuestionInfo = new ViewQuestionInfo((Question)(((Button)sender).DataContext));
+        //    viewQuestionInfo.ShowDialog();
+        //}
 
-        private void Low_Checked(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.ComplexityCheck.Execute(Complexity.Low);
-        }
+        //private void Low_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.ComplexityCheck.Execute(Complexity.Low);
+        //}
 
-        private void Normal_Checked(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.ComplexityCheck.Execute(Complexity.Medium);
+        //private void Normal_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.ComplexityCheck.Execute(Complexity.Medium);
 
-        }
+        //}
 
-        private void Hight_Checked(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.ComplexityCheck.Execute(Complexity.Hight);
-        }
+        //private void Hight_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.ComplexityCheck.Execute(Complexity.Hight);
+        //}
 
-        private void SubjectsBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            testBlankViewModel.ChooseSubject.Execute(SubjectsBox.SelectedItem.ToString());
-        }
+        //private void SubjectsBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    testBlankViewModel.ChooseSubject.Execute(SubjectsBox.SelectedItem.ToString());
+        //}
 
-        private void MinutesForExamTimeTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !(Char.IsDigit(e.Text, 0));
-        }
+        //private void MinutesForExamTimeTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        //{
+        //    e.Handled = !(Char.IsDigit(e.Text, 0));
+        //}
 
-        private void AllQuestioView_Click(object sender, RoutedEventArgs e)
-        {
-            ViewQuestionInfo viewQuestionInfo = new ViewQuestionInfo((Question)(((Button)sender).DataContext));
-            viewQuestionInfo.ShowDialog();
-        }
+        //private void AllQuestioView_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ViewQuestionInfo viewQuestionInfo = new ViewQuestionInfo((Question)(((Button)sender).DataContext));
+        //    viewQuestionInfo.ShowDialog();
+        //}
 
-        private void AllQuestioAdd_Click(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.AddQuestionToTestBlank.Execute((Question)(((Button)sender).DataContext));
-        }
+        //private void AllQuestioAdd_Click(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.AddQuestionToTestBlank.Execute((Question)(((Button)sender).DataContext));
+        //}
 
-        private void SubmitButton_Click(object sender, RoutedEventArgs e)
-        {
-            testBlankViewModel.TestBlank.About = TestBlk.About;
-            testBlankViewModel.TestBlank.Name = TestBlk.Name;
-            testBlankViewModel.TestBlank.DurationMin = TestBlk.DurationMin;
-            testBlankViewModel.TestBlank.Autor = TestBlk.Autor;
-            testBlankViewModel.AddTestBlank.Execute(true);
-            MessageBox.Show("Natig sdelay cto nibud pole najatiya cnopki ctobi bilo ponatno sozdalsa ili net", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
+        //private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    testBlankViewModel.TestBlank.About = TestBlk.About;
+        //    testBlankViewModel.TestBlank.Name = TestBlk.Name;
+        //    testBlankViewModel.TestBlank.DurationMin = TestBlk.DurationMin;
+        //    testBlankViewModel.TestBlank.Autor = TestBlk.Autor;
+        //    testBlankViewModel.AddTestBlank.Execute(true);
+        //    MessageBox.Show("Natig sdelay cto nibud pole najatiya cnopki ctobi bilo ponatno sozdalsa ili net", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Information);
+        //}
 
-        private void NameTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TestBlk.Name = NameTextBox.Text;
-        }
+        //private void NameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    TestBlk.Name = NameTextBox.Text;
+        //}
 
-        private void AboutTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TestBlk.About = AboutTextBox.Text;
-        }
+        //private void AboutTextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    TestBlk.About = AboutTextBox.Text;
+        //}
 
-        private void AuthorTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
+        //private void AuthorTextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
 
-            TestBlk.Autor = AuthorTextBox.Text;
+        //    TestBlk.Autor = AuthorTextBox.Text;
 
-        }
+        //}
 
-        private void MinutesForExamTimeTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TestBlk.DurationMin = new TimeSpan(0, Convert.ToInt32(MinutesForExamTimeTextBox.Text), 0);
+        //private void MinutesForExamTimeTextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    TestBlk.DurationMin = new TimeSpan(0, Convert.ToInt32(MinutesForExamTimeTextBox.Text), 0);
 
-        }
+        //}
     }
 }
